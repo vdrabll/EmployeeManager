754093243ee15531706d483e7e4ae4a8e0d3c336
vdrabll <fedosova.univ@gmail.com>
2024-09-16 13:14:38 +0300

    init data fix

--- src/main/resources/db/changelog/changesets/v2_initial_data.sql
+++ src/main/resources/db/changelog/changesets/v2_initial_data.sql
@@ -1,24 +1,23 @@
 
-INSERT INTO employee (is_working_now, full_name, email, role) VALUES (TRUE, 'Симонова Мария Кайна', 'Mary@sber.ru', 'EMPLOYEE');
-INSERT INTO employee (is_working_now, full_name, email, role) VALUES (TRUE, 'Стоматин Петр Петрович', 'ILoveTwirin@google.com', 'EMPLOYEE');
-INSERT INTO employee (is_working_now, full_name, email, role) VALUES (TRUE, 'Полежайкина Жанна Дмитриевна', 'jelannay@sber.ru', 'CHIEF');
+INSERT INTO department (id, name, location) VALUES (1, 'Отдел продаж', 'Москва');
+INSERT INTO department (id, name, location) VALUES (2, 'Отдел разработки', 'Новосибирск');
+INSERT INTO department (id, name, location) VALUES (3,                                                                                                                                                                                                          'Финансовый отдел', 'Ангора');
 
 
-INSERT INTO department (name, location) VALUES ('Отдел продаж', 'Москва');
-INSERT INTO department (name, location) VALUES ('Отдел разработки', 'Новосибирск');
-INSERT INTO department (name, location) VALUES ('Финансовый отдел', 'Москва');
+INSERT INTO position (id, grade, salary, name) VALUES (1, 1, 50000, 'Младший специалист');
+INSERT INTO position (id, grade, salary, name) VALUES (2, 2, 70000, 'Специалист');
+INSERT INTO position (id, grade, salary, name) VALUES (3, 3, 90000, 'Ведущий специалист');
+INSERT INTO position (id, grade, salary, name) VALUES (4, 4, 120000, 'Руководитель отдела');
 
-
-INSERT INTO position (grade, salary, name) VALUES (1, 50000, 'Младший специалист');
-INSERT INTO position (grade, salary, name) VALUES (2, 70000, 'Специалист');
-INSERT INTO position (grade, salary, name) VALUES (3, 90000, 'Ведущий специалист');
-INSERT INTO position (grade, salary, name) VALUES (4, 120000, 'Руководитель отдела');
+INSERT INTO employee (id, is_working_now, full_name, email, role_name) VALUES (1, TRUE, 'Симонова Мария Кайна', 'Mary@sber.ru', 'EMPLOYEE');
+INSERT INTO employee (id, is_working_now, full_name, email, role_name) VALUES (2, TRUE, 'Стоматин Петр Петрович', 'ILoveTwirin@google.com', 'EMPLOYEE');
+INSERT INTO employee (id, is_working_now, full_name, email, role_name) VALUES (3, TRUE, 'Полежайкина Жанна Дмитриевна', 'jelannay@sber.ru', 'CHIEF');
 
 INSERT INTO position_history (employee_id, start_date, end_date, position_id) VALUES (1, '2022-01-01', NULL, 1);
 INSERT INTO position_history (employee_id, start_date, end_date, position_id) VALUES (2, '2022-02-01', NULL, 2);
 
-INSERT INTO project (name, description) VALUES ('Проект A', 'Первый проект компании');
-INSERT INTO project (name, description) VALUES ('Проект B', 'Второй проект компании');
+INSERT INTO project (id, name, description) VALUES (1, 'Проект A', 'Первый проект компании');
+INSERT INTO project (id, name, description) VALUES (2, 'Проект B', 'Второй проект компании');
 
 INSERT INTO schedule (employee_id, date, start_time, end_time, location) VALUES (1, '2022-01-01', '2022-01-01 09:00:00', '2022-01-01 18:00:00', 'OFFICE');
 INSERT INTO schedule (employee_id, date, start_time, end_time, location) VALUES (2, '2022-01-01', '2022-01-01 09:00:00', '2022-01-01 18:00:00', 'OFFICE');
@@ -38,4 +37,4 @@ INSERT INTO salary_history (employee_id, salary_date, amount, type) VALUES (2, '
 INSERT INTO task (name, description, priority, employee_id, deadline, status, type, project_id) VALUES ('Задача 1', 'Описание первой задачи', 'HIGH', 1, '2022-01-01', 'IN_WORK', 'Разработка', 1);
 INSERT INTO task (name, description, priority, employee_id, deadline, status, type, project_id) VALUES ('Задача 2', 'Описание второй задачи', 'MEDIUM', 2, '2022-01-01', 'IN_WORK', 'Аналитика', 2);
 
-INSERT INTO salary_coefficients (year, advance_percentage, bonus_percentage) VALUES ('2024-01-01', 30.00, 40.00);
+INSERT INTO salary_coefficients (advance_percentage, bonus_percentage) VALUES (0.30, 0.40);
